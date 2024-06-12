6a2d78dbfe3ca0f966e436916903832882ee0443
itallo2L <itallocerqueiracosta@gmail.com>
2024-06-12 12:49:06 -0300

    Mudei a classe Program.cs do Forms para pública

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cod3rsGrowth.Forms
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
