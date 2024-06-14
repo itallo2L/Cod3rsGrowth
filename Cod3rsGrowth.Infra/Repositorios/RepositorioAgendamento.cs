63d590452d5941aa0edc2286ef2c2eb5746cf2bc
itallo2L <itallocerqueiracosta@gmail.com>
2024-06-14 13:36:45 -0300

    Adicionado a conexão com o BD, o arquivo App.config no Forms e as classes de repositório

---
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioAgendamento.cs
@@ -0,0 +1,35 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+
+namespace Cod3rsGrowth.Infra.Repositorios
+{
+    public class RepositorioAgendamento : IRepositorioAgendamento
+    {
+        public void Adicionar(Agendamento agendamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Agendamento agendamentoParaAtualizar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Agendamento ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Agendamento> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioEstudioMusical.cs
@@ -0,0 +1,35 @@
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+
+namespace Cod3rsGrowth.Infra.Repositorios
+{
+    public class RepositorioEstudioMusical : IRepositorioEstudioMusical
+    {
+        public void Adicionar(EstudioMusical estudioMusical)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(EstudioMusical estudioParaAtualizar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EstudioMusical ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EstudioMusical> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
