d69fb46ca9567c58f9995fa9bfb352dc5249efd9
itallo2L <itallocerqueiracosta@gmail.com>
2024-06-17 10:57:55 -0300

    Atualizado um nome errado na classe "BdCod3rsGrowth" e os arquivos csproj no gitignore

--- Cod3rsGrowth.Infra/BdCod3rsGrowth.cs
+++ Cod3rsGrowth.Infra/BdCod3rsGrowth.cs
@@ -6,7 +6,7 @@ namespace Cod3rsGrowth.Infra
     public class BdCod3rsGrowth : LinqToDB.Data.DataConnection
     {
         public BdCod3rsGrowth() { }
-        public ITable<Agendamento> Agendameno => this.GetTable<Agendamento>();
+        public ITable<Agendamento> Agendamento => this.GetTable<Agendamento>();
         public ITable<EstudioMusical> EstudioMusical => this.GetTable<EstudioMusical>();
     }
 }
