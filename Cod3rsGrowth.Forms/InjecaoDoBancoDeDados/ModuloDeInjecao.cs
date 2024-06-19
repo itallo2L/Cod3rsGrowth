b0b56955a59ea36c9345ec1745bad81905cec58a
itallo2L <itallocerqueiracosta@gmail.com>
2024-06-19 09:14:37 -0300

    Editado o nome da variável de ambient
    e

--- Cod3rsGrowth.Forms/InjecaoDoBancoDeDados/ModuloDeInjecao.cs
+++ Cod3rsGrowth.Forms/InjecaoDoBancoDeDados/ModuloDeInjecao.cs
@@ -12,7 +12,7 @@ namespace Cod3rsGrowth.Forms.InjecaoDoBancoDeDados
         public static void AdicionarDependenciasNoEscopo(ServiceCollection servico)
         {
             servico.AddLinqToDBContext<BdCod3rsGrowth>((provider, options) => options
-            .UseSqlServer(Configuration.GetConnectionString("Cod3rsGrowth_BD")));
+            .UseSqlServer(Configuration.GetConnectionString("ConnectionCod3rsGrowth")));
         }
     }
 }
