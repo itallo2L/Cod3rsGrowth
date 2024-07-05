78ec579a83904d395d7a092e2dc5a7e0659b0bf0
itallo2L <itallocerqueiracosta@gmail.com>
2024-07-05 12:28:10 -0300

    Adicionei o FormCadastrarEstudioMusical

---
+++ Cod3rsGrowth.Forms/FormCadastrarEstudioMusical.cs
@@ -0,0 +1,17 @@
+﻿using Cod3rsGrowth.Dominio.Servicos;
+using Cod3rsGrowth.Servico.Servicos;
+
+namespace Cod3rsGrowth.Forms
+{
+    public partial class FormCadastrarEstudioMusical : Form
+    {
+        private readonly ServicoAgendamento _servicoAgendamento;
+        private readonly ServicoEstudioMusical _servicoEstudioMusical;
+        public FormCadastrarEstudioMusical(ServicoAgendamento servicoAgendamento, ServicoEstudioMusical servicoEstudioMusical)
+        {
+            _servicoAgendamento = servicoAgendamento;
+            _servicoEstudioMusical = servicoEstudioMusical;
+            InitializeComponent();
+        }
+    }
+}
