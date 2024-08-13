5894e676c0977761c6a69b04c5ec03375f0d50f7
itallo2L <itallocerqueiracosta@gmail.com>
2024-08-13 14:02:51 -0300

    Adicionei mais dois testes OPA

--- Cod3rsGrowth.Web/wwwroot/test/integration/pages/Estudio.js
+++ Cod3rsGrowth.Web/wwwroot/test/integration/pages/Estudio.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    'sap/ui/test/Opa5'
+    'sap/ui/test/Opa5',
+	'sap/ui/test/matchers/AggregationLengthEquals',
+	'sap/ui/test/matchers/I18NText',
+    'sap/ui/test/actions/Press'
 ],
-    function (Opa5) {
+    function (Opa5, AggregationLengthEquals, I18NText, Press) {
         "use strict";
 
-        const viewName = "ui5.cod3rsgrowth.app.estudio.Estudio";
+        const viewName = "ui5.cod3rsgrowth.app.estudio.Estudio.view";
 
         Opa5.createPageObjects({
             naPaginaListaEstudio: {
@@ -17,9 +20,54 @@ sap.ui.define([
                             viewName: viewName,
                             success: () => Opa5.assert.ok(true, "A tela de listagem carregou corretamente"),
                             errorMessage: "A tela de listagem não carregou corretamente"
-                        })
-                    }
-                }
+                        });
+                    },
+
+                    aListaDeveTerPaginacao: function () {
+					return this.waitFor({
+						viewName: viewName,
+						matchers: new AggregationLengthEquals({
+							name: "items",
+							length: 20
+						}),
+						success: function () {
+							Opa5.assert.ok(true, "A Lista contém 20 estúdios na primeira página.");
+						},
+						errorMessage: "A Lista não contém todos os estúdios."
+					});
+				},
+
+				aPaginaDeveConterTodosOsEstudios: function () {
+					return this.waitFor({
+						viewName: viewName,
+						matchers: new AggregationLengthEquals({
+							name: "items",
+							length: 25
+						}),
+						success: function () {
+							Opa5.assert.ok(true, "A Lista tem 25 estúdios.");
+						},
+						errorMessage: "A Lista não contém todos os estúdios."
+					});
+				},
+
+				oTituloDeveMostrarOTamanhoTotalDeItens: function () {
+					return this.waitFor({
+						id: "idListaEstudio",
+						viewName: viewName,
+						matchers: new I18NText({
+							key: "worklistTableTitleCount",
+							propertyName: "text",
+							parameters: [25]
+						}),
+						success: function () {
+							Opa5.assert.ok(true, "O cabeçalho da lista tem 25 estúdios");
+						},
+						errorMessage: "O cabeçalho da lista não contém o número de estúdios: 25"
+					});
+				}
+                    
             }
-        });
+        }
     });
+});
--- Cod3rsGrowth.Web/wwwroot/test/integration/pages/JornadaEstudio.js
+++ Cod3rsGrowth.Web/wwwroot/test/integration/pages/JornadaEstudio.js
@@ -7,19 +7,35 @@ sap.ui.define([
 	QUnit.module("Lista Estúdio");
 
 		opaTest("Deve carregar a tela de listagem do Estúdio", function (Given, When, Then) {
-			// Arrangements
 			Given.iStartMyUIComponent({
 				componentConfig: {
 					name: "ui5.cod3rsgrowth"
 				}
 			});
 	
-			// Assertions
 			Then
 				.naPaginaListaEstudio
 				.aTelaFoiCarregadaCorretamente();
-
-			Then.
-				iTeardownMyApp();
 		});
+			
+		opaTest("Deve mostrar 25 Estúdios listados na view do Estúdio", function (Given, When, Then) {
+			Then
+				.naPaginaListaEstudio
+				.aListaDeveTerPaginacao()
+				.and.oTituloDeveMostrarOTamanhoTotalDeItens();
+
+		});
+
+		opaTest("Deve carregar mais itens", function (Given, When, Then) {
+			When
+				.naPaginaListaEstudio
+				.euClicoEmMaisDados();
+
+			Then
+				.naPaginaListaEstudio
+				.aPaginaDeveConterTodosOsEstudios();
+
+			Then
+				.iTeardownMyApp();
+		})
 });
